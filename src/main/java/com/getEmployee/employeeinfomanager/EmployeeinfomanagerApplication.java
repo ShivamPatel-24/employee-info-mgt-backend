ed232b741f880c27690a612e169f75687a90fd80
Shivam <shivampatel2473@gmail.com>
2022-08-07 18:41:22 -0400

    setup project Api codebase

---
+++ src/main/java/com/getEmployee/employeeinfomanager/EmployeeinfomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.getEmployee.employeeinfomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeinfomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeinfomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getEmployee/employeeinfomanager/EmployeeinfomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getEmployee.employeeinfomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeinfomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
